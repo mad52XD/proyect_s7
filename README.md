b509a6d3835cc26602bc2c2753fc3ef1b118c1e5
mad52XD <blisterndsun@hotmail.com>
2025-06-22 15:39:02 -0600

    Primer commit app v1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # proyect_s7
 Tripleten sprint 7 proyect
+This project uses provided car data to create two infographics, one scatter plot and one histogram.
+The code is written in python and uses streamlit to generate the web application.
---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.header("Interactive Data Visualization with Plotly and Streamlit")
+st.write("This app allows you to visualize data interactively using Plotly and Streamlit.")
+
+car_data = pd.read_csv("/home/mad52xd/proyect_s7/vehicles_us.csv")
+hist_button= st.button("Show Histogram")
+scatter_button = st.button("Show Scatter Plot")
+    
+if hist_button:
+    st.write("Histogram of Vehicles Odometer Readings")
+    fig = px.histogram(car_data, x="odomter")
+    st.plotly_chart(fig, use_container_width=True)
+
+elif scatter_button:
+    st.write("Scatter Plot of Vehicles Odometer Readings vs Price")
+    fig = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
+    fig.show()
