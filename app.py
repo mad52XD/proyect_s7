346662e2a4deb210461ca72117ad750c7a7fcbde
mad52XD <blisterndsun@hotmail.com>
2025-06-22 16:49:48 -0600

    Updated for two histograms

--- app.py
+++ app.py
@@ -6,16 +6,16 @@ st.header("Interactive Data Visualization with Plotly and Streamlit")
 st.write("This app allows you to visualize data interactively using Plotly and Streamlit.")
 
 car_data = pd.read_csv("/home/mad52xd/proyect_s7/vehicles_us.csv")
-hist_button= st.button("Show Histogram")
-scatter_button = st.button("Show Scatter Plot")
-'''
-if hist_button:
-    st.write("Histogram of Vehicles Odometer Readings")
-    fig = px.histogram(car_data, x="odomter")
-    st.plotly_chart(fig, use_container_width=True)
+hist_button_o= st.button("Show Mileaage Histogram")
+hist_button_p = st.button("Show Prices Histogram")
 
-elif scatter_button:
-    st.write("Scatter Plot of Vehicles Odometer Readings vs Price")
-    fig = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
-    fig.show()
-'''
+if hist_button_o:
+    st.subheader("Histogram of Mileage")
+    fig = px.histogram(car_data, x="odometer", nbins=50, title="Vehicle Price Distribution")
+    st.plotly_chart(fig)
+
+if hist_button_p:
+    st.subheader("Histogram of Prices")
+    fig = px.histogram(car_data, x="price", nbins=50, title="Vehicle Price Distribution")
+    st.plotly_chart(fig)
+
